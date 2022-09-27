93e6b4da7e17cbd64fef6e5895f9227490c669c0
Jasurbek-948 <uz.web.coder@gmail.com>
2022-09-27 13:50:09 +0500

    mainPage

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MainPage = () =>{
+    return(
+        <div>
+            <h1>Welcome to Wapik</h1>
+            <p>Wapik is multipurpose template, with modern and smart design. Wapik is perfect template for you!</p>
+        </div>
+    )
+}
