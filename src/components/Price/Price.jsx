d687746a1dc2e7178a58fecd50eba63a860855d4
Jasurbek-948 <uz.web.coder@gmail.com>
2022-10-01 16:20:16 +0500

    tick

--- src/components/Price/Price.jsx
+++ src/components/Price/Price.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import './Price.css'
+import tick from '../image/tick.png'
 const Price = () => {
     return (
         <div>
@@ -9,44 +10,89 @@ const Price = () => {
                 <div className="box-text text-center" style={{width: "480px"}}>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.
                     Donec id elit non mi porta gravida at eget metus.</div>
             </div>
-            <div className="row">
+            <div className="container">
+
+
+            <div className="row my-5">
                 <div className="col">
-                    <div>
-                        <div>BASIC</div>
-                        <div>$199</div>
-                        <div>/month</div>
-                        <div>Justo Fringilla
-                            Mollis Sit Nullam
-                            Ultricies Purus Amet
-                            Cras Inceptos
+                    <div className="price">
+                        <div className="premium">BASIC</div>
+                        <div className="pirce-inner">$199</div>
+                        <div className="month">/month</div>
+                        <div><div className="box-text my-3">Justo Fringilla</div>
+                            <div className="box-text my-3">Mollis Sit Nullam</div>
+                            <div className="box-text my-3">Ultricies Purus Amet</div>
+                            <div className="box-text my-3">Cras Inceptos</div>
                         </div>
-                        <button>SELECT PLAN</button>
+                        <button className="plan">SELECT PLAN</button>
                     </div>
                 </div>
                 <div className="col">
-                    <div>
-                        <div>PLUS</div>
-                        <div>$399</div>
-                        <div>/month</div>
-                        <div>Justo Fringilla
-                            Mollis Sit Nullam
-                            Ultricies Purus Amet
-                            Cras Inceptos
+                    <div className="price">
+                        <div className="premium">PLUS</div>
+                        <div className="pirce-inner">$399</div>
+                        <div className="month">/month</div>
+                        <div>
+                            <div className="box-text my-3">Justo Fringilla</div>
+                            <div className="box-text my-3">Mollis Sit Nullam</div>
+                            <div className="box-text my-3">Ultricies Purus Amet</div>
+                            <div className="box-text my-3">Cras Inceptos</div>
+
                         </div>
-                        <button>SELECT PLAN</button>
+                        <button className="plan">SELECT PLAN</button>
                     </div>
                 </div>
                 <div className="col">
-                    <div>
-                        <div>PREMIUM</div>
-                        <div>$699</div>
-                        <div>/month</div>
-                        <div>Justo Fringilla
-                            Mollis Sit Nullam
-                            Ultricies Purus Amet
-                            Cras Inceptos
+                    <div className="price">
+                        <div className="premium">PREMIUM</div>
+                        <div className="pirce-inner">$699</div>
+                        <div className="month">/month</div>
+                        <div><div className="box-text my-3">Justo Fringilla</div>
+                            <div className="box-text my-3">Mollis Sit Nullam</div>
+                            <div className="box-text my-3">Ultricies Purus Amet</div>
+                            <div className="box-text my-3">Cras Inceptos</div>
                         </div>
-                        <button>SELECT PLAN</button>
+                        <button className="plan">SELECT PLAN</button>
+                    </div>
+                </div>
+
+            </div>
+
+                <div>
+                    <div className="title">Included with all Plans</div>
+                    <div className="line1"></div>
+                    <div className="row">
+                        <div className="col">
+                            <div className="price-tick">
+                                <div>
+                                    <img src={tick} alt=""/>
+                                </div>
+                                <div>
+                                    Maecenas faucibus mollis interdum
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="col">
+                            <div className="price-tick">
+                                <div>
+                                    <img src={tick} alt=""/>
+                                </div>
+                                <div>
+                                    Vestibulum id ligula porta felis euismod
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="price-tick">
+                                <div>
+                                    <img src={tick} alt=""/>
+                                </div>
+                                <div>
+                                    Nulla vitae elit libero, a pharetra augue
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
