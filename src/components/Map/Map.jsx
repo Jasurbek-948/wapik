cd0940083d75098030b1c8cecd02bbe879e183ac
Jasurbek-948 <uz.web.coder@gmail.com>
2022-10-01 14:42:15 +0500

    map

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Map = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <div>Built with High Attention to Details</div>
+                        <div>
+                            Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean lacinia bibendum nulla sed consectetur. Maecenas sed diam eget risus varius blandit sit amet non magna. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus.
+                        </div>
+                    </div>
+                    <div className="col"></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Map;
