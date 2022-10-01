6fe258dd0b02039f0529fc11bdfd58a7743c01e4
Jasurbek-948 <uz.web.coder@gmail.com>
2022-10-01 17:40:00 +0500

    email

---
+++ src/components/wapik/Wapik.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Wapik = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <div>Wapik</div>
+                        <div>@: hi@wapik.com</div>
+                        <div>p: +62 202 555 0117</div>
+                        <div>a: 610 Overlook Circle Suite 323</div>
+                        <div>Kalamazoo, MI 49009</div>
+                    </div>
+                    <div className="col">
+                        <div>Company</div>
+                        <div>Home</div>
+                        <div>About Us</div>
+                        <div>Pricing</div>
+                        <div>Contact Us</div>
+                    </div>
+                    <div className="col">
+                        <div>Others</div>
+                        <div>Help & Support
+                        </div>
+                        <div>Privacy Policy</div>
+                        <div>Terms</div>
+                        <div>Sitemap</div>
+                    </div>
+                    <div className="col">
+                        <div>Newsletter</div>
+                        <div>Subscribe to our newsletter and get all
+                            the cool news</div>
+                        <input type="email" placeholder="Enter Email"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Wapik;
