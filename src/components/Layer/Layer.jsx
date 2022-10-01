79ec89905b167c9edeefb6b5599cfbc68ec60c42
Jasurbek-948 <uz.web.coder@gmail.com>
2022-10-01 13:55:53 +0500

    layer

---
+++ src/components/Layer/Layer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Layer = () => {
+  return (
+    <div>
+      <p>This theme is beautiful, elegant and easy to customize. I’ve been able to build an amazing site in just a minute. Thank you very much for creating this impressive template!</p>
+        <p>Valeria Boltneva
+            Director of Acme.Inc</p>
+    </div>
+  )
+}
+
+export default Layer
